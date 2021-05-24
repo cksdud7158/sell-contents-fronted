d566a792eb09bdc4ac05c22b8332e6bbb73a1a75
cksdud7158 <cksdud7158@naver.com>
2021-05-24 19:30:23 +0900

    21.05.24

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,8 +3,18 @@ import React from "react";
 import { isLoggedInVar } from "../apollo";
 import { LoggedInRouter } from "../routers/logged-in-router";
 import { LoggedOutRouter } from "../routers/logged-out-router";
+import { HeaderComponet } from "../components/header";
 
 export const App = () => {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
-  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
+  return (
+    <div className="h-screen">
+      <div className="absolute w-full">
+        <HeaderComponet temp="11"></HeaderComponet>
+      </div>
+      <div className="h-full pt-14">
+        {isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />}
+      </div>
+    </div>
+  );
 };
---
+++ src/pages/common_pages/mainPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MainPage = () => {
+  return (
+    <div className="w-full bg-red-200">
+      <h1>mainPage</h1>
+    </div>
+  );
+};
